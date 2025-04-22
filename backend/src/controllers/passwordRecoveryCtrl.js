cb64dd4a39ebb32a3c1dddc964199a0416b78ade
AlexDev07 <alekeymurcia@gmail.com>
2025-04-22 09:59:58 -0600

    feat: mejorar la lógica de recuperación de contraseña y manejo de errores

--- backend/src/controllers/passwordRecoveryCtrl.js
+++ backend/src/controllers/passwordRecoveryCtrl.js
@@ -1,14 +1,44 @@
 import jsonwebtoken from 'jsonwebtoken';
 import bcryptjs from 'bcryptjs';
+import crypto from 'crypto';
 
 import customersMdl from '../models/customersmdl.js';
 import employeesMdl from '../models/employeesmdl.js';
 
-import{config} from '../config.js';
+import { config } from '../config.js';
 import { sendEmail, HTMLRecoveryEmail } from '../utils/mailPasswordRecovery.js';
 
 const passwordRecoveryCtrl = {};
 
-passwordRecoveryCtrl.requestCode = async(req,res) => {
-    
+passwordRecoveryCtrl.requestCode = async (req, res) => {
+    const { email } = req.body;
+    try {
+        let userFound;
+        let userType;
+
+        userFound = await customersMdl.findOne({ email });
+        if (userFound) {
+            userType = 'customer';
+        }
+        else {
+            userFound = await employeesMdl.findOne({ email });
+            if (userFound) {
+                userType = 'employee';
+            }
+        }
+
+        if (!userFound) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const verificationCode = crypto.randomBytes(3).toString('hex');
+
+        const token = jsonwebtoken.sign(
+            { email, verificationCode, userType, verified: false }, 
+            config.jwt.JWT_SECRET, 
+            { expiresIn: '30s' }
+        );
+    } catch (error) {
+
+    }
 }
